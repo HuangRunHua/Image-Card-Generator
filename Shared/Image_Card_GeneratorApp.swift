1c308e9329838fe34c6dd0a992368c05c1d02b95
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2021-07-11 22:30:08 +0800

    Initial Commit

---
+++ Shared/Image_Card_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_Card_GeneratorApp.swift
+//  Shared
+//
+//  Created by Runhua Huang on 2021/7/11.
+//
+
+import SwiftUI
+
+@main
+struct Image_Card_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
